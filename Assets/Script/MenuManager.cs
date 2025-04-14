fa992ab5dbeda8fc7de1171ccec203f26cd84589
Ianperezching <152056248+Ianperezching@users.noreply.github.com>
2025-04-14 17:05:26 -0500

    waza

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Text Text;
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
